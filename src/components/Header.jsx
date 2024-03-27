37ffa08b7a3fd8b52b8464cde12b1b380b8c8e6d
seoyoung <915jin@gmail.com>
2024-03-27 16:55:01 +0900

    헤더 슬라이드

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import MainLogo from '../assets/MainLogo.svg';
 import './header.scss';
 
 
-const Header = () => {
-    const activeStyle={
-        fontWeight:'bold',
-        color:'#000'
-    }
+ const Header = () => {
+
+    const [isHovering, setIsHovering] = useState(false);
 
     return (
         <header id="headerGnb" className='gnbNav up'>
@@ -23,39 +21,45 @@ const Header = () => {
                     <div className='form mo'></div>
                 </div>
                 <nav className="menu">
-                    <ul className="depth1">
-                        <li className="menuList on"><NavLink to="/"style={({isActive}) => (isActive? activeStyle : undefined)}>홈</ NavLink></li>
-                        <li className="menuList"><NavLink to="/main/theme.do"style={({isActive}) => (isActive? activeStyle : undefined)}>테마</ NavLink></li>
-                        <li className="menuList"><NavLink to="/main/area.do" style={({isActive}) => (isActive? activeStyle : undefined)}>지역</ NavLink></li>
-                        <li className="menuList"><NavLink to="#" style={({isActive}) => (isActive? activeStyle : undefined)}>여행콕콕</ NavLink>
-                            <ul className='depth2'>
-                                <li className="depth2List"><NavLink to="#">AI콕콕</ NavLink></li>
-                                <li className="depth2List"><NavLink to="#"></NavLink>AI콕콕 플래너</li>
-                            </ul></li>
-                        <li className="menuList"><NavLink to="/tgpr/tgpr_main.do" style={({isActive}) => (isActive? activeStyle : undefined)} >여행상품홍보관</ NavLink></li>
-                        <li className="menuList"><NavLink to="/tgpr/tgpr_main.do" style={({isActive}) => (isActive? activeStyle : undefined)}>여행정보</ NavLink>
-                            <ul className="depth2">
-                                <li className="depth2List"><NavLink to="#">여행지</ NavLink></li>
-                                <li className="depth2List"><NavLink to="#"></ NavLink>여행기사</li>
-                                <li className="depth2List"><NavLink to="#"></ NavLink>여행코스</li>
-                                <li className="depth2List"><NavLink to="#"></ NavLink>축제</li>
-                                <li className="depth2List"><NavLink to="#"></ NavLink>공연 / 행사</li>
-                                <li className="depth2List"><NavLink to="#"></NavLink>이벤트</li>
-                                <li className="depth2List"><NavLink to="#"></NavLink>가볼래&#45;터</li>
-                                <li className="depth2List"><NavLink to="#"></NavLink>디지털관광주민증</li>
-                            </ul>
-                        </li>
-                        <li className="menuList"><NavLink to="#" style={({isActive}) => (isActive? activeStyle : undefined)}>여행지도</ NavLink></li>
+                    <ul className="depth1"  onMouseOver={()=>setIsHovering(true)}  onMouseOut={() => setIsHovering(false)} >
+                        <li className="menuList"><NavLink to="/" className={({isActive}) => (isActive? 'on' : '')}>홈</ NavLink></li>
+                        <li className="menuList"><NavLink to="/main/theme.do" className={({isActive}) => (isActive? 'on' : '')}>테마</ NavLink></li>
+                        <li className="menuList"><NavLink to="/main/area.do" className={({isActive}) => (isActive? 'on' : '')}>지역</ NavLink></li>
+                        <li className="menuList"><NavLink to="/main/plan.do" className={({isActive}) => (isActive? 'on' : '')}>여행콕콕</ NavLink></li>
+                        <li className="menuList"><NavLink to="/tgpr/tgpr_main.do" className={({isActive}) => (isActive? 'on' : '')}>여행상품 홍보관</ NavLink></li>
+                        <li className="menuList"><NavLink to="/tgpr/tgpr_plan.do" className={({isActive}) => (isActive? 'on' : '')}>여행정보</ NavLink></li>
+                        <li className="menuList"><NavLink to="/main/map.do" className={({isActive}) => (isActive? 'on' : '')}>여행지도</ NavLink></li>
                     </ul>
+                    {
+                        isHovering? (
+                           <div className='depthback' onMouseOver={()=>setIsHovering(true)}  onMouseOut={() => setIsHovering(false)}>
+                                <ul className="depth2 plan">
+                                    <li className="depth2List"><p>AI콕콕</ p></li>
+                                    <li className="depth2List"><p>핫플콕콕</ p></li>
+                                    <li className="depth2List"><p>AI콕콕 플래너</p></li> 
+                                </ul>
+                                <ul className="depth2 tgpr">    
+                                    <li className="depth2List"><p>여행지</p></li>
+                                    <li className="depth2List"><p>여행기사</p></li>
+                                    <li className="depth2List"><p>여행코스</p></li>
+                                    <li className="depth2List"><p>축제</p></li>
+                                    <li className="depth2List"><p>공연 / 행사</p></li>
+                                    <li className="depth2List"><p>이벤트</p></li>
+                                    <li className="depth2List"><p>가볼래&#45;터</p></li>
+                                    <li className="depth2List"><p>디지털관광주민증</p></li>
+                                    
+                                </ul>
+                           </div>
+                            
+                            
+                        ):("")
+                    }
                 </nav>
                 <div className='form pc'>
                     <span className='input' id='placeHolder'>
                         <input type="text" placeholder='어디로,어떤 여행을 떠날 예정인가요?'id='inp_search' />
-                        <NavLink to="#">검색</NavLink>
-                    </span>
-                    <div className="profile">
-                        <NavLink to="#">로그인</NavLink>
-                    </div>
+                        <Link>검색</Link></span>
+                    <div className="profile"><Link>로그인</Link></div>
 
                 </div>
                 
